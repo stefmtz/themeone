175a41dbb29d5f83ce29267b81136302fb8fbbfa
Stef M <Stef@Stef.local>
2017-07-23 18:28:04 +0200

    Style posts in home

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,20 +10,32 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-featured">
+		<a href="<?php echo esc_url(get_permalink()); ?>">
+			<?php
+				the_post_thumbnail();
+			?>
+		</a>	
+	</div>
 	<header class="entry-header">
 		<?php
+		if ( 'post' === get_post_type() ) : ?>
+			<h4 class="entry-meta entry-category">
+				<?php 
+				foreach((get_the_category()) as $category) { 
+    				echo '<a href='. get_category_link($category->cat_ID) .'>'.$category->category_nicename. '</a>'; 
+				} ?>
+			</h4>
+		<?php
+		endif;
+
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php themeone_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -31,7 +43,7 @@
 			the_content( sprintf(
 				wp_kses(
 					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'themeone' ),
+					__( 'Seguir leyendo &#187; <span class="screen-reader-text"> "%s"</span>', 'themeone' ),
 					array(
 						'span' => array(
 							'class' => array(),
@@ -48,7 +60,7 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php themeone_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<!--<footer class="entry-footer">
+		<?php //themeone_entry_footer(); ?>
+	</footer> .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
